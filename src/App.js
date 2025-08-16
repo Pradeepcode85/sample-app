ba6acc1d290ed63461556fbc647feb7620d03393
Pradeepcode85 <pradeepsofteon@gmail.com>
2025-08-16 22:22:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Brand from "./page/Brand";
+import Distributor from "./page/Distributor";
+import Retailer from "./page/Retailer";
+ 
+import Confirmation from './page/Confirmation';
+import FormComponent from './page/FormComponent';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+     <Router>
+      <Routes>
+        <Route path="/form" element={<FormComponent />} />
+        <Route path="/confirmation" element={<Confirmation />} />
+      </Routes>
+    </Router>
   );
 }
 
